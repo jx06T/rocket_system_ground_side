7ffe7168c623d964d9fe478a386e34ea49989d1f
jx06t <50313tjx06@gmail.com>
2025-02-17 09:57:55 +0800

    檔案整理

--- src/gui/ui_main.py
+++ src/gui/ui_main.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file 't1.ui'
+# Form implementation generated from reading ui file 'src/gui/ui_main.ui'
 #
 # Created by: PyQt6 UI code generator 6.8.1
 #
